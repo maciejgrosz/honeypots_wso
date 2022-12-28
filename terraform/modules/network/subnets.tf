b0c01d004d940577bcc41c359b6c648ae9715fa9
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-12-28 21:06:23 +0100

    working cluster D:DD

---
+++ terraform/modules/network/subnets.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "subnets" {
+  count = 2
+  vpc_id                  = aws_vpc.maciejgroszyk_tf_vpc.id
+  cidr_block              = var.subnets[count.index]["cidr"]
+  availability_zone       = var.subnets[count.index]["az"]
+  tags                    = var.subnets[count.index]["tags"]
+  map_public_ip_on_launch = var.subnets[count.index]["public_ip"]
+}
---
+++ terraform/modules/network/vars.tf
@@ -0,0 +1,37 @@
+variable "route_table" {
+}
+
+variable "internet_gateway_tags" {
+
+}
+variable "ingress_cidr_blocks" {
+
+}
+variable "ingress_rules" {
+
+}
+variable "egress_cidr_blocks" {
+
+}
+
+variable "egress_rules" {
+
+}
+variable "rules" {
+
+}
+
+variable "security_group_tag" {
+
+}
+variable "subnets" {
+
+}
+
+variable "tf_vpc" {
+
+}
+
+variable "single_config" {
+    
+}
