ee08cdc7af84a7f0f0fab4edbb9c93b93b383f54
Maciej Groszyk <maciej.groszyk8@gmail.com>
2023-01-15 17:06:09 +0100

    working version

--- terraform/modules/compute/eks-cluster.tf
+++ terraform/modules/compute/eks-cluster.tf
@@ -24,3 +24,12 @@ resource "aws_eks_node_group" "node" {
     min_size     = var.no_of_nodes
   }
 }
+
+# data "tls_certificate" "eks_cluster" {
+#   url = aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer
+# }
+# resource "aws_iam_openid_connect_provider" "eks_cluster" {
+#   client_id_list = ["sts.amazonaws.com"]
+#   thumbprint_list = concat([data.tls_certificate.eks_cluster.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
+#   url = aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer
+# }
